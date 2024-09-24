5ab7bb8cb2e1a901f9ac7d0500e99dc35c753c0c
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2024-09-25 00:22:24 +0530

    Added Shutdown and Sleep functionality

--- Frontend/src/context/auth.jsx
+++ Frontend/src/context/auth.jsx
@@ -8,31 +8,32 @@ export const useAuth = () => useContext(AuthContext);
 
 // AuthProvider component that wraps the app and provides the auth state
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
+	const [isShutdown, setIsShutdown] = useState(false);
 
-  // Load user from localStorage when the app initializes
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+	// Load user from localStorage when the app initializes
+	useEffect(() => {
+		const storedUser = localStorage.getItem("user");
+		if (storedUser) {
+			setUser(JSON.parse(storedUser));
+		}
+	}, []);
 
-  // Function to handle login and save user in localStorage
-  const login = (username) => {
-    const newUser = { username };
-    setUser(newUser);
-    localStorage.setItem("user", JSON.stringify(newUser));
-  };
+	// Function to handle login and save user in localStorage
+	const login = (username) => {
+		const newUser = { username };
+		setUser(newUser);
+		localStorage.setItem("user", JSON.stringify(newUser));
+	};
 
-  // Function to handle logout and remove user from localStorage
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-  };
+	// Function to handle logout and remove user from localStorage
+	const logout = () => {
+		setUser(null);
+		localStorage.removeItem("user");
+	};
 
-  // Authentication value provided to context consumers
-  const value = { user, login, logout };
+	// Authentication value provided to context consumers
+	const value = { user, login, logout, isShutdown, setIsShutdown };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,47 +2,64 @@ import Draggable from "react-draggable";
 import Logo from "../assets/JioCloudPC.png";
 import { useNavigate } from "react-router-dom";
 import { MdShortcut } from "react-icons/md";
+import { useAuth } from "../context/auth";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const [fadeOut, setFadeOut] = useState(false);
+	const { isShutdown, setIsShutdown, logout } = useAuth();
 
-  const handleDrag = (e, position) => {
-    const { x, y } = position;
-    // console.log(`Current position: X: ${x}, Y: ${y}`);
-  };
-  return (
-    <div className="w-full h-full bg-gradient-to-r from-red-500 via-orange-400 to-pink-600">
-      <Draggable
-        bounds="parent"
-        onDrag={handleDrag}
-        defaultPosition={{ x: 25, y: 60 }}
-      >
-        <div
-          className="w-[100px] h-[85px] text-center"
-          // style={{
-          // 	cursor: isDragging ? "move" : "pointer",
-          // }}
-          onDoubleClick={() => {
-            navigate("/accops/dashboard");
-          }}
-          // onDragStart={() => {
-          // 	setIsDragging(true);
-          // }}
-          // onDragEnd={() => {
-          // 	setIsDragging(false);
-          // }}
-        >
-          <img
-            src={Logo}
-            alt="JioCloudPc logo"
-            className="w-[50px] h-[50px] rounded-lg block mx-auto mb-3 "
-          />
-          <MdShortcut className="absolute top-10 left-4 bg-blue-600 rounded-sm w-5" />
-          JioCloudPC
-        </div>
-      </Draggable>
-    </div>
-  );
+	function handleFadeOutClick() {
+		setIsShutdown(false);
+		setFadeOut(true);
+		setTimeout(() => {
+			setFadeOut(false);
+		}, 1000);
+		logout();
+		navigate("/");
+	}
+
+	return (
+		<>
+			{isShutdown && (
+				<>
+					<audio src="/audio/shutdown.mp3" autoPlay />
+					<div
+						className={`fixed top-0 left-0 w-full h-full bg-black transition-opacity duration-1000 ease-in-out ${
+							fadeOut ? "opacity-0" : "opacity-100"
+						} z-50`}
+						onClick={handleFadeOutClick}>
+						<div className="flex flex-col gap-4 justify-center items-center w-full h-screen">
+							<img
+								src="/chromeshutdown.png"
+								alt="Random"
+								className="w-1/2 h-1/2 object-cover rounded-lg shadow-lg"
+							/>
+							<div className="text-white">BYE BYE👋🏻</div>
+						</div>
+					</div>
+				</>
+			)}
+			<div className="w-full h-full bg-gradient-to-r from-red-500 via-orange-400 to-pink-600">
+				<Draggable bounds="parent" defaultPosition={{ x: 25, y: 60 }}>
+					<div
+						className="w-[100px] h-[85px] text-center"
+						onDoubleClick={() => {
+							navigate("/accops/dashboard");
+						}}>
+						<img
+							src={Logo}
+							alt="JioCloudPc logo"
+							className="w-[50px] h-[50px] rounded-lg block mx-auto mb-3 "
+						/>
+						<MdShortcut className="absolute top-10 left-4 bg-blue-600 rounded-sm w-5" />
+						JioCloudPC
+					</div>
+				</Draggable>
+			</div>
+		</>
+	);
 };
 
 export default Home;
