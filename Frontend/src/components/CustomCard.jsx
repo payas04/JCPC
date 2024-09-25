9b432c8a6d8c25eafcc6b63517bb804a2140eae1
binitkumarshirsath <binitkumar0911@gmail.com>
2024-09-25 17:31:40 +0530

    add user cards

--- Frontend/src/components/CustomCard.jsx
+++ Frontend/src/components/CustomCard.jsx
@@ -2,9 +2,9 @@ import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
 
 export function CustomCard() {
   return (
-    <CardContainer className="inter-var">
-      <CardBody className="card mx-auto mt-40 h-64  w-72 border rounded-sm flex-col flex items-center border-black bg-gradient-to-br from-[#5558da] to-[#5fd1f9] text-white shadow-xl hover:shadow">
-        <CardItem translateZ="100" className="w-full">
+    <CardContainer className="py-0 h-64 mx-auto">
+      <CardBody className="card  h-64 py-0  w-72 border px-2 mx-2 rounded-sm flex-col flex items-center border-black bg-gradient-to-br from-[#5558da] to-[#5fd1f9] text-white shadow-xl hover:shadow">
+        <CardItem translateZ="65" className="w-full">
           <img
             className="mx-auto -mt-36 w-36 rounded-full border-8 border-white outline  outline-black"
             alt="thumbnail"
--- Frontend/src/pages/Accops/JioCloudPc.jsx
+++ Frontend/src/pages/Accops/JioCloudPc.jsx
@@ -8,24 +8,12 @@ const JioCloudPc = () => {
       {/* Sidebar */}
       <Sidebar />
 
-      {/* <div className="mt-10 w-full flex">
-        <div className="card mx-auto mt-40 w-72 border rounded-sm flex-col flex items-center border-black bg-white shadow-xl hover:shadow">
-          <img
-            className="mx-auto -mt-28 w-32 rounded-full border-8 border-white"
-            src="https://avatars.githubusercontent.com/u/67946056?v=4"
-            alt
-          />
-          <div className="mt-2 text-center text-3xl font-medium">Ajo Alex</div>
-          <div className="mt-2 text-center text-sm font-light">@devpenzil</div>
-          <div className="text-center text-lg font-normal">Kerala</div>
-          <div className="mt-2 px-3 text-center text-sm font-light">
-            <p>
-              Front end Developer, avid reader. Love to take a long walk, swim
-            </p>
-          </div>
-        </div>
-      </div> */}
-      <CustomCard />
+      <div className="mt-28 w-full mx-auto  flex px-11  justify-between">
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+      </div>
     </div>
   );
 };
