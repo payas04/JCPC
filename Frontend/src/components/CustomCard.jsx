83b7c678cc5dbf9c9f99964c90b59e96d6c6a882
binitkumarshirsath <binitkumar0911@gmail.com>
2024-09-25 17:45:53 +0530

    made small card changes

--- Frontend/src/components/CustomCard.jsx
+++ Frontend/src/components/CustomCard.jsx
@@ -2,9 +2,9 @@ import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
 
 export function CustomCard() {
   return (
-    <CardContainer className="py-0 h-64 mx-auto">
+    <CardContainer className="py-0 mb-10 h-64 mx-auto">
       <CardBody className="card  h-64 py-0  w-72 border px-2 mx-2 rounded-sm flex-col flex items-center border-black bg-gradient-to-br from-[#5558da] to-[#5fd1f9] text-white shadow-xl hover:shadow">
-        <CardItem translateZ="65" className="w-full">
+        <CardItem translateZ="64" className="w-full">
           <img
             className="mx-auto -mt-36 w-36 rounded-full border-8 border-white outline  outline-black"
             alt="thumbnail"
--- Frontend/src/pages/Accops/JioCloudPc.jsx
+++ Frontend/src/pages/Accops/JioCloudPc.jsx
@@ -4,14 +4,14 @@ import data from "../../db.json";
 
 const JioCloudPc = () => {
   return (
-    <div className="flex h-screen w-full flex-1 bg-white">
+    <div className="flex h-screen w-full flex-1  bg-white">
       {/* Sidebar */}
       <Sidebar />
 
-      <div className="mt-28 w-full mx-auto  flex px-11  justify-between">
-        <CustomCard />
-        <CustomCard />
-        <CustomCard />
+      <div className=" w-full pt-20 border-2 border-black mx-auto  overflow-y-scroll grid grid-cols-4 px-11  ">
+        {data.map((user, index) => {
+          return <CustomCard key={index} />;
+        })}
         <CustomCard />
       </div>
     </div>
