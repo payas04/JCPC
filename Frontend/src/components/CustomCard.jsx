2229d8435ad25cc02270899bc9ef84b7d7d9c781
sameep25 <sameepvishwakarma@gmail.com>
2024-09-26 15:46:01 +0530

    creates userModal

--- Frontend/src/components/CustomCard.jsx
+++ Frontend/src/components/CustomCard.jsx
@@ -1,24 +1,42 @@
 import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
+import { useState } from "react";
+import UserModal from "./UserModal";
 
-export function CustomCard() {
+export function CustomCard({ user, shouldOpenModal = false }) {
+  const [open, setOpen] = useState(false);
+  const handleOpenModal = () => {
+    console.log(open);
+    if (!shouldOpenModal) {
+      setOpen(true);
+    }
+  };
+
   return (
-    <CardContainer className="py-0 mb-10 h-64 mx-auto">
-      <CardBody className="card  h-64 py-0  w-72 border px-2 mx-2 rounded-sm flex-col flex items-center border-black bg-gradient-to-br from-[#5558da] to-[#5fd1f9] text-white shadow-xl hover:shadow">
-        <CardItem translateZ="64" className="w-full">
-          <img
-            className="mx-auto -mt-36 w-36 rounded-full border-8 border-white outline  outline-black"
-            alt="thumbnail"
-            src="/images/Binit.jpg"
-          />
-        </CardItem>
-
-        <CardItem
-          translateZ="60"
-          className="text-white text-lg max-w-sm mt-2 text-center font-semibold"
-        >
-          Hover over this card to unleash the power of CSS perspective
-        </CardItem>
-      </CardBody>
-    </CardContainer>
+    <div onClick={handleOpenModal}>
+      <UserModal open={open} setOpen={setOpen} user={user} />
+      <CardContainer className="py-0 mb-10 h-64 mx-auto mt-8">
+        <CardBody className="card  h-64 py-0  w-72 border px-2 mx-2 rounded-lg flex-col flex items-center text-black bg-[#bfe8f2] shadow-xl hover:shadow">
+          {/* bg-gradient-to-br from-[#92d1f8] to-[#bbedff] */}
+          <CardItem translateZ="64" className="w-full">
+            {/* Image */}
+            <img
+              className="mx-auto -mt-36 w-36 rounded-full border-8 border-white outline-none "
+              alt="thumbnail"
+              src="/images/Binit.jpg"
+            />
+          </CardItem>
+          {/* Info */}
+          <div className="flex flex-col justify-center items-center">
+            <span className="font-semibold">{user && user.Name}</span>
+            <span>@developer</span>
+            <span>Issue count : 100</span>
+          </div>
+          <CardItem
+            translateZ="60"
+            className="text-white text-lg max-w-sm mt-2 text-center font-semibold"
+          ></CardItem>
+        </CardBody>
+      </CardContainer>
+    </div>
   );
 }
--- Frontend/src/pages/Accops/JioCloudPc.jsx
+++ Frontend/src/pages/Accops/JioCloudPc.jsx
@@ -10,7 +10,7 @@ const JioCloudPc = () => {
 
       <div className=" w-full pt-20 border-2 border-black mx-auto  overflow-y-scroll grid grid-cols-4 px-11  ">
         {data.map((user, index) => {
-          return <CustomCard key={index} />;
+          return <CustomCard key={index} user={user} />;
         })}
         <CustomCard />
       </div>
