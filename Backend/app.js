fb5c149d39a6e0b92a08b475f2155eb4d2917360
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2024-11-17 18:02:03 +0530

    Backend updated

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ dotenv.config({
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: process.env.FRONTEND_URL,
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 		credentials: true,
 		allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,15 +1,18 @@
-let mongoose = require("mongoose")
+let mongoose = require("mongoose");
 
-let connectDB = async ()=>{
-try{
-    let connect = await mongoose.connect("mongodb+srv://hammzza1605:admin@hamzacluster.74l3k.mongodb.net/hza?retryWrites=true&w=majority&appName=HamzaCluster")
-    console.log("Connection established", connect.connection.host,connect.connection.name)
-}
-catch(err){
-    console.log(err);
-    // console.log("sdfsdf") 
-    process.exit(1)  
-}
-}
+let connectDB = async () => {
+	try {
+		let connect = await mongoose.connect(process.env.CONNECTION_STRING);
+		console.log(
+			"Connection established",
+			connect.connection.host,
+			connect.connection.name
+		);
+	} catch (err) {
+		console.log(err);
+		// console.log("sdfsdf")
+		process.exit(1);
+	}
+};
 
-module.exports= connectDB;
+module.exports = connectDB;
