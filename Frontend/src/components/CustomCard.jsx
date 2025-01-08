f275fd359a309f1971c6ac5870cea71e7a39cc26
sameep25 <sameepvishwakarma@gmail.com>
2025-01-08 11:34:38 +0530

    Team section Fix

--- Frontend/src/components/CustomCard.jsx
+++ Frontend/src/components/CustomCard.jsx
@@ -3,57 +3,66 @@ import { useState } from "react";
 import UserModal from "./modals/UserModal";
 
 export default function CustomCard({ user, allUsers }) {
-	const [isOpen, setIsOpen] = useState(false);
-	const handleOpenModal = (e) => {
-		e.stopPropagation();
-		setIsOpen(true);
-	};
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpenModal = (e) => {
+    e.stopPropagation();
+    setIsOpen(true);
+  };
 
-	const handleCloseModal = () => {
-		setIsOpen(false);
-	};
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
 
-	return (
-		<>
-			<div
-				onClick={handleOpenModal}
-				className="group bg-[#F8F9FA] rounded-lg border cursor-pointer shadow-lg transition-all duration-300  hover:-translate-y-2 hover:bg-blue-300">
-				<CardContainer className="flex flex-col w-full justify-center mx-2 my-4 text-center  ">
-					<CardItem
-						translateZ="60"
-						className="flex flex-col justify-center items-center w-full">
-						<img
-							alt={user.name}
-							className="w-24 h-24 mb-4 object-cover rounded-full ring-2 ring-blue-800 ring-offset-2 transition-all duration-300 group-hover:ring-4 bg-white "
-							src={user.image}
-							onError={(e) => {
-								e.target.src = "/images/profile/default.png";
-							}}
-						/>
+  return (
+    <>
+      <div
+        onClick={handleOpenModal}
+        className="group bg-[#F8F9FA] rounded-lg border cursor-pointer shadow-lg transition-all duration-300  hover:-translate-y-2 hover:bg-blue-300"
+      >
+        <CardContainer className="flex flex-col w-full justify-center mx-2 my-4 text-center  ">
+          <CardItem
+            translateZ="60"
+            className="flex flex-col justify-center items-center w-full"
+          >
+            <img
+              alt={user.name}
+              className="w-24 h-24 mb-4 object-cover rounded-full ring-2 ring-blue-800 ring-offset-2 transition-all duration-300 group-hover:ring-4 bg-white "
+              src={user.image}
+              onError={(e) => {
+                e.target.src = "/images/profile/default.png";
+              }}
+            />
 
-						<span className="text-lg flex flex-col font-semibold leading-tight">
-							{user && user.name}
-						</span>
-						<span
-							className={` text-sm font-semibold ${
-								user.tag === "Released" ? "text-gray-500" : "text-blue-800"
-							} `}>
-							{user.role}
-						</span>
-						<div className="mt-4 bg-blue-800 text-white rounded-full px-3 py-1 text-sm font-medium w-fit self-center duration-700">
-							Score: {user.score.current}
-						</div>
-					</CardItem>
-				</CardContainer>
-			</div>
-			{isOpen && (
-				<UserModal
-					open={isOpen}
-					setOpen={handleCloseModal}
-					initialUser={user}
-					users={allUsers} // Pass all users for carousel functionality
-				/>
-			)}
-		</>
-	);
+            <span className="text-lg flex flex-col font-semibold leading-tight">
+              {user &&
+                (() => {
+                  const [firstName, lastName] = user.name.split(" ");
+                  return user.name.length > 15
+                    ? `${firstName} ${lastName ? lastName[0] : ""}`
+                    : user.name;
+                })()}
+            </span>
+            <span
+              className={` text-sm font-semibold ${
+                user.tag === "Released" ? "text-gray-500" : "text-blue-800"
+              } `}
+            >
+              {user.role}
+            </span>
+            <div className="mt-4 bg-blue-800 text-white rounded-full px-3 py-1 text-sm font-medium w-fit self-center duration-700">
+              Score: {user.score.current}
+            </div>
+          </CardItem>
+        </CardContainer>
+      </div>
+      {isOpen && (
+        <UserModal
+          open={isOpen}
+          setOpen={handleCloseModal}
+          initialUser={user}
+          users={allUsers} // Pass all users for carousel functionality
+        />
+      )}
+    </>
+  );
 }
--- Frontend/src/components/charts/BarGraph.jsx
+++ Frontend/src/components/charts/BarGraph.jsx
@@ -1,50 +1,51 @@
 import React from "react";
 import {
-	BarChart,
-	Bar,
-	XAxis,
-	YAxis,
-	CartesianGrid,
-	Tooltip,
-	Legend,
-	ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 const BarGraph = ({ barData }) => {
-	console.log(barData[0].name);
+  console.log(barData[0].name);
 
-	return (
-		<div style={{ width: "100%", height: 200, maxWidth: 800 }}>
-			<ResponsiveContainer>
-				<BarChart
-					width={500}
-					height={300}
-					data={barData}
-					margin={{
-						top: 5,
-						right: 30,
-						left: 20,
-						bottom: 5,
-					}}>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="name" />
-					<YAxis />
-					<Tooltip />
-					<Legend />
-					<Bar
-						dataKey={Object.keys(barData[0])[1]}
-						fill="#82B7CA" //#E4C087
-						// activeBar={<Rectangle fill="pink" stroke="blue" />}
-					/>
-					<Bar
-						dataKey={Object.keys(barData[0])[2]}
-						fill="#82ca9d" //#BC7C7C
-						// activeBar={<Rectangle fill="gold" stroke="purple" />}
-					/>
-				</BarChart>
-			</ResponsiveContainer>
-		</div>
-	);
+  return (
+    <div style={{ width: "100%", height: 200, maxWidth: 800 }}>
+      <ResponsiveContainer>
+        <BarChart
+          width={500}
+          height={300}
+          data={barData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          {/* <Tooltip /> */}
+          <Legend />
+          <Bar
+            dataKey={Object.keys(barData[0])[1]}
+            fill="#82B7CA" //#E4C087
+            // activeBar={<Rectangle fill="pink" stroke="blue" />}
+          />
+          <Bar
+            dataKey={Object.keys(barData[0])[2]}
+            fill="#82ca9d" //#BC7C7C
+            // activeBar={<Rectangle fill="gold" stroke="purple" />}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default BarGraph;
